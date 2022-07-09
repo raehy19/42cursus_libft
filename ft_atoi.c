2bd30195231de3bcfa4256b09bfd76e79d755583
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-09 20:51:30 +0900

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/09 20:34:36 by rjeong            #+#    #+#             */
+/*   Updated: 2022/07/09 20:50:52 by rjeong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_delete_whitespace(char *str, int *i)
+{
+	while ((8 < *(str + *i) && *(str + *i) < 14) || *(str + *i) == ' ')
+		++(*i);
+}
+
+int	ft_cal_sign(char *str, int *i)
+{
+	int	sign;
+
+	sign = 1;
+	if (ft_isdigit(*(str + *i)))
+	{
+		if (*(str + *i) == '-')
+			sign = -1;
+		++(*i);
+	}
+	return (sign);
+}
+
+int	atoi(const char *str)
+{
+	int			i;
+	int			sign;
+	long long	result;
+
+	i = 0;
+	ft_delete_whitespace(str, &i);
+	sign = ft_cal_sign(str, &i);
+	result = 0;
+	while (ft_isdigit(*(str + i)))
+	{
+		result = result * 10 + (long long)(*(str + i) - '0');
+		++i;
+	}
+	result = result * sign;
+	return ((int)result);
+}
