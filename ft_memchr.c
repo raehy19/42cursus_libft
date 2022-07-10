8a37fd0d4b1c31dec93bb8c8051886e8c4c63695
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-10 16:16:39 +0900

    fix : fix find value 0 case error

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:57:54 by rjeong            #+#    #+#             */
-/*   Updated: 2022/07/10 15:52:01 by rjeong           ###   ########.fr       */
+/*   Updated: 2022/07/10 16:15:49 by rjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (*(((unsigned char *) s) + i) && i < n)
 	{
-		if (*(((unsigned char *) s) + i) == (char) c)
+		if (*(((unsigned char *) s) + i) == (unsigned char) c)
 			return ((((unsigned char *) s) + i));
 		++i;
 	}
+	if (c == 0 && i < n)
+		return ((((unsigned char *) s) + i));
 	return (NULL);
 }
