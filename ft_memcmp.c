efbe2f6e75558e00586ac983e5dc765f043cd685
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-10 17:20:08 +0900

    fix : fix int underflow error

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 19:55:45 by rjeong            #+#    #+#             */
-/*   Updated: 2022/07/10 16:35:31 by rjeong           ###   ########.fr       */
+/*   Updated: 2022/07/10 17:19:17 by rjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n - 1)
+	while (i + 1 < n)
 	{
 		if (*(((unsigned char *) s1) + i) != *(((unsigned char *) s2) + i))
 			break ;
