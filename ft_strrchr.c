709b853fca0919496e362b1b1ac20a88e3411189
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-10 16:44:32 +0900

    fix : fix find value 0 case error

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:52:51 by rjeong            #+#    #+#             */
-/*   Updated: 2022/07/10 15:52:16 by rjeong           ###   ########.fr       */
+/*   Updated: 2022/07/10 16:43:51 by rjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
-	i = ft_strlen(s) - 1;
+	i = ft_strlen(s);
 	while (i > -1)
 	{
 		if (*(s + i) == (char) c)
