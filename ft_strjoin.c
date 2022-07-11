912a57057738414708a73fd2fe0b02eb11984ade
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-11 11:27:31 +0900

    refactor : change function memcpy -> memmove

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 21:29:13 by rjeong            #+#    #+#             */
-/*   Updated: 2022/07/10 21:53:46 by rjeong           ###   ########.fr       */
+/*   Updated: 2022/07/11 11:26:41 by rjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	dst = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!dst)
 		return (NULL);
-	ft_memcpy(dst, s1, s1_len);
-	ft_memcpy(dst + s1_len, s2, s2_len + 1);
+	ft_memmove(dst, s1, s1_len);
+	ft_memmove(dst + s1_len, s2, s2_len + 1);
 	return (dst);
 }
