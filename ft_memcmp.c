b83733f23a8742c653c56e60f6773afe2301d825
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-13 14:43:33 +0900

    fix : fix size 0 case error

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 19:55:45 by rjeong            #+#    #+#             */
-/*   Updated: 2022/07/10 17:19:17 by rjeong           ###   ########.fr       */
+/*   Updated: 2022/07/13 14:43:10 by rjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i + 1 < n)
+	while (i < n)
 	{
 		if (*(((unsigned char *) s1) + i) != *(((unsigned char *) s2) + i))
-			break ;
+			return ((int)(*(((unsigned char *) s1) + i)
+				- *(((unsigned char *) s2) + i)));
 		++i;
 	}
-	return ((int)(*(((unsigned char *) s1) + i)
-		- *(((unsigned char *) s2) + i)));
+	return (0);
 }
