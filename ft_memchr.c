08c90497443be5e2025789a79504d3276a981397
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-13 22:23:51 +0900

    fix : delete \0 condition

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:57:54 by rjeong            #+#    #+#             */
-/*   Updated: 2022/07/10 16:15:49 by rjeong           ###   ########.fr       */
+/*   Updated: 2022/07/13 22:23:14 by rjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (*(((unsigned char *) s) + i) && i < n)
+	while (i < n)
 	{
 		if (*(((unsigned char *) s) + i) == (unsigned char) c)
 			return ((((unsigned char *) s) + i));
 		++i;
 	}
-	if (c == 0 && i < n)
-		return ((((unsigned char *) s) + i));
 	return (NULL);
 }
