45c892e0ab40adc02217667269fc5ec57d5858a6
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-14 19:23:20 +0900

    fix : fix type cast

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:21:29 by rjeong            #+#    #+#             */
-/*   Updated: 2022/07/10 16:42:50 by rjeong           ###   ########.fr       */
+/*   Updated: 2022/07/14 19:22:56 by rjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strchr(const char *s, int c)
 			return ((char *)(s + i));
 		++i;
 	}
-	if (c == 0)
+	if ((char) c == '\0')
 		return ((char *)(s + i));
 	return (NULL);
 }
