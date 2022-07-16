d9a4583f85d39ac4d9f8f655ef30263eb7d47469
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-16 17:50:44 +0900

    fix : fix function f do memory alloc case

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,27 +12,40 @@
 
 #include "libft.h"
 
+void	*ft_mem_alloc_fail(t_list **head, void (*del)(void *))
+{
+	ft_lstclear(&head, del);
+	return (NULL);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
 	t_list	*temp_read;
 	t_list	*temp_write;
+	void	*temp_content;
 
 	if (!lst)
 		return (NULL);
 	temp_read = lst;
-	temp_write = ft_lstnew(f(temp_read->content));
+	temp_content = f(temp_read->content);
+	if (!temp_content)
+		return (NULL);
+	temp_write = ft_lstnew(temp_content);
 	if (!temp_write)
 		return (NULL);
 	head = temp_write;
 	while (temp_read->next)
 	{
 		temp_read = temp_read->next;
-		temp_write->next = ft_lstnew(f(temp_read->content));
+		temp_content = f(temp_read->content);
+		if (!temp_content)
+			return (ft_mem_alloc_fail(&lst, del));
+		temp_write = ft_lstnew(temp_content);
 		if (!(temp_write->next))
 		{
-			ft_lstclear(&head, del);
-			return (NULL);
+			free(temp_content);
+			return (ft_mem_alloc_fail(&lst, del));
 		}
 		temp_write = temp_write->next;
 	}
